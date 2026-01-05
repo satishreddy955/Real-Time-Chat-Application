6555edb5643792a2c344050d4ffdb6396c2a27aa
satishreddy955 <168215588+satishreddy955@users.noreply.github.com>
2026-01-05 08:51:52 +0530

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000", {
+const socket = io("https://real-time-chat-application-1-0hnv.onrender.com", {
   transports: ["websocket"],
   autoConnect: false
 });
